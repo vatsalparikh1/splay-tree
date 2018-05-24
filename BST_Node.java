a83449d9979e5c4d8f87a59b2c5e3fb82429550a
vatsalparikh1 <vatsal@email.unc.edu>
2018-05-24 17:33:26 -0400

    Update BST_Node.java

--- BST_Node.java
+++ BST_Node.java
@@ -4,9 +4,9 @@ public class BST_Node {
   String data;
   BST_Node left;
   BST_Node right;
-  BST_Node par; //parent...not necessarily required, but can be useful in splay tree
-  boolean justMade; //could be helpful if you change some of the return types on your BST_Node insert.
-            //I personally use it to indicate to my SPLT insert whether or not we increment size.
+  BST_Node par; 
+  boolean justMade; 
+  
   
   BST_Node(String data){ 
     this.data=data;
@@ -21,27 +21,13 @@ public class BST_Node {
     this.justMade=true;
   }
 
-  // --- used for testing  ----------------------------------------------
-  //
-  // leave these 3 methods in, as is (meaning also make sure they do in fact return data,left,right respectively)
+  
 
   public String getData(){ return data; }
   public BST_Node getLeft(){ return left; }
   public BST_Node getRight(){ return right; }
 
-  // --- end used for testing -------------------------------------------
-
   
-  // --- Some example methods that could be helpful ------------------------------------------
-  //
-  // add the meat of correct implementation logic to them if you wish
-
-  // you MAY change the signatures if you wish...names too (we will not grade on delegation for this assignment)
-  // make them take more or different parameters
-  // have them return different types
-  //
-  // you may use recursive or iterative implementations
-
   
   public BST_Node containsNode(String s){//if contains=> return this; if !contains => parent;
 		if(data.equals(s))return this;
@@ -53,8 +39,8 @@ public class BST_Node {
 			if(right==null)return this;
 			return right.containsNode(s);
 		}
-		return null; //shouldn't hit
-  } //note: I personally find it easiest to make this return a Node,(that being the node splayed to root), you are however free to do what you wish.
+		return null; 
+  } 
   
   public BST_Node insertNode(String s){
 
@@ -111,12 +97,10 @@ public class BST_Node {
 	  }
 	  
 	  
-  } //you could have this return or take in whatever you want..so long as it will do the job internally. As a caller of SPLT functions, I should really have no idea if you are "splaying or not"
-                        //I of course, will be checking with tests and by eye to make sure you are indeed splaying
-                        //Pro tip: Making individual methods for rotateLeft and rotateRight might be a good idea!
+  } 
   
 
-  // --- end example methods --------------------------------------
+
 
   private void rotateLeft(BST_Node c){ //when c is left child of parent
 	  BST_Node p = c.par;
@@ -195,10 +179,6 @@ public class BST_Node {
 	  
   }
   
-
-  // --------------------------------------------------------------------
-  // you may add any other methods you want to get the job done
-  // --------------------------------------------------------------------
   
   
 }
